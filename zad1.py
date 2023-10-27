61745b04aa9e018aa992ce441ddf126654918478
MikIwoTur <147725952+MikIwoTur@users.noreply.github.com>
2023-10-27 20:27:02 +0200

    Add files via upload

---
+++ zad1.py
@@ -0,0 +1,25 @@
+import re
+
+table_data = """
+UUU F      CUU L      AUU I      GUU V
+UUC F      CUC L      AUC I      GUC V
+UUA L      CUA L      AUA I      GUA V
+UUG L      CUG L      AUG M      GUG V
+UCU S      CCU P      ACU T      GCU A
+UCC S      CCC P      ACC T      GCC A
+UCA S      CCA P      ACA T      GCA A
+UCG S      CCG P      ACG T      GCG A
+UAU Y      CAU H      AAU N      GAU D
+UAC Y      CAC H      AAC N      GAC D
+UAA Stop   CAA Q      AAA K      GAA E
+UAG Stop   CAG Q      AAG K      GAG E
+UGU C      CGU R      AGU S      GGU G
+UGC C      CGC R      AGC S      GGC G
+UGA Stop   CGA R      AGA R      GGA G
+UGG W      CGG R      AGG R      GGG G
+"""
+lista_kodonow = re.findall('[UCAG][UCAG][UCAG](?#)', table_data)
+lista_aminokwasow = re.findall('(?<=[UCAG][UCAG][UCAG])..', table_data)
+
+slownik = dict(zip(lista_kodonow, lista_aminokwasow))
+print(slownik)
